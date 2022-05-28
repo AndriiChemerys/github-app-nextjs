4c8c3d822bd25dddc9a0e2e1dc55b6083e7915d9
Andrii Chemerys <andriichemerys@gmail.com>
2022-05-28 11:14:11 +0200

    Building App with Next

---
+++ components/sections/header.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+export default function Header() {
+  return(
+    <header>
+      Hello footer
+      <nav> 
+        <ul>
+          <li>
+            <Link href="/">Index</Link>
+          </li>
+          <li>
+            <Link href="/results">Results</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  )
+}
---
+++ pages/results.js
@@ -0,0 +1,25 @@
+import Head from "next/head"
+
+import Main from "@/components/layouts/main"
+
+export default function ResultsPage({title}) {
+  return (
+    <Main> 
+      <Head>
+      <title>Results page for {title}</title>
+      </Head>
+      <h1> Hello Results </h1>
+    </Main>
+  )
+}
+
+export async function getServerSideProps(context) {
+
+  // tutaj jest miejsce na zapytanie asynchroniczne
+
+  return {
+    props: {
+      title: 'Bicycle'
+    },
+  }
+}
